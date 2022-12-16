1f68bab2a5ecf5eb6a7e56c530c89ca8c6e6b3d4
Timur0794 <curseofelo@gmail.com>
2022-12-16 07:28:17 +0600

    admin.category

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Создание категории</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- Main content -->
+    <div class="row">
+        <div class="col-12">
+            <form action ="{{route('category.store')}}" class="w-25" method="POST">
+                @csrf
+                @method('POST')
+                <div class="form-group">
+                    <input type="text" class="form-control" name="title" placeholder="Название категории">
+                </div>
+                <input type="submit" class="btn btn-primary" value="Добавить">
+            </form>
+
+        </div>
+
+    </div>
+    <!-- /.content -->
+@endsection
---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.main')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Категории</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row col-2">
+                <a href="{{route('category.create')}}" class="btn btn-block btn-primary">Создать</a>
+            </div>
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Категории</h3>
+                        <div class="card-tools">
+                            <div class="input-group input-group-sm" style="width: 150px;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Наименование</th>
+                                <th>Удалить</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                @foreach($categories as $category)
+                                    <td>{{$category->id}}</td>
+                                    <td><a href="{{route('category.edit', $category->id)}}">{{$category->title}}</a></td>
+                                    <td>
+                                        <form action="{{route('category.delete', $category->id)}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="border-0 bg-transparent">
+                                                <i class="fas fa-trash text-danger"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                @endforeach
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
